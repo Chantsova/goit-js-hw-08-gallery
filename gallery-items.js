9c43bf1a3d77f374015353935c4c178be0ce676e
Chantsova Anastasia <stasyadiv@gmail.com>
2021-06-14 01:14:53 +0300

    Add modal, keys, overlay functions

--- gallery-items.js
+++ gallery-items.js
@@ -64,15 +64,27 @@ export default [
   },
 ];
 
+
+// 4. Если хотите выполнить задание в части реализации пролистывания изображений, при создании разметки путем дополнения тега img data-атрибутом записывайте для каждого из изображений его индекс (разумеется, листание можно реализовать и путем навигации по DOM либо прохождением циклом по массиву всех изображений, но это куда менее оптимальный вариант).
+
+// 5. Слушатели для закрытия модального окна по клавише ESC и листания изображений создавайте только при открытии модального окна (то есть внутри ответственной за это функции). Соответственно, удаляйте при закрытии (внутри функции, отвечающей за закрытие модалки) – память браузера скажет вам за это спасибо. (edited) 
+
 import images from './gallery-items.js';
 
-const imagesBox = document.querySelector('.js-gallery');
-const bodyEl = document.querySelector('body');
+const refs = {
+  imagesBox: document.querySelector('.js-gallery'),
+  originalImage: document.querySelector('.gallery__link'),
+  lightboxEl: document.querySelector('.lightbox'),
+  lightboxImg: document.querySelector('.lightbox__image'),
+  lightboxCloseBtn: document.querySelector('.lightbox__button'),
+  lightboxOverlay: document.querySelector('.lightbox__overlay'),
+}
 
 const imagesCardsMarkup = makeImageCard(images);
-imagesBox.insertAdjacentHTML('beforeend', imagesCardsMarkup);
+refs.imagesBox.insertAdjacentHTML('beforeend', imagesCardsMarkup);
 
 function makeImageCard(images) {
+  
   return images
     .map(({preview, original, description}) => {
       return `
@@ -86,27 +98,54 @@ function makeImageCard(images) {
     .join('');
 };
 
-bodyEl.addEventListener('click', onImageClick);
-  
-function onImageClick(event) {
-  event.preventDefault()
-  const isImageBoxEl = event.target.classList.contains('a');
 
+
+function onImageClick(event) {
+  event.preventDefault();
+  
+  window.addEventListener('keydown', onEscKeyPress);
+  window.addEventListener('keydown', onNavigationKeysPress);
+  const isImageBoxEl = event.target.classList.contains('gallery__image');
   if (!isImageBoxEl) {
     return;
   }
-
-  const currentActiveImageBox = document.querySelector('.lightbox.is-open');
-
-  // if(currentActiveImageBox) {
-  //   currentActiveImageBox.classList.remove(is-open);
-  // }
   
-  // const lightboxEl = document.querySelector('.lightbox');
-  // lightboxEl.classList.add('is-open');
+  refs.lightboxEl.classList.add('is-open');
 
-  //const lightboxImg = document.querySelector('.lightbox__image');
-  
-  //lightboxImg.getAttribute(src) = 
+  const source = event.target.dataset.source;
+  const altName = event.target.getAttribute('alt');
+  refs.lightboxImg.setAttribute('src', source);
+  refs.lightboxImg.setAttribute('alt', altName);
+  let index = images.map(image => image.original).indexOf(source);
+  return index;
+  }
+
+function forCloseModal(event) {
+  window.removeEventListener('keydown', onEscKeyPress);
+  window.removeEventListener('keydown', onNavigationKeysPress);
+  refs.lightboxEl.classList.remove('is-open');
+  refs.lightboxImg.setAttribute('src', '');
+  refs.lightboxImg.setAttribute('alt', '');
 }
-  
+
+function onEscKeyPress(event) {
+  const ESC_KEY_CODE = 'Escape';
+  const isEscKey = event.code === ESC_KEY_CODE;
+  if (isEscKey) {
+    forCloseModal();
+  }
+}
+
+function onNavigationKeysPress(event) {
+  if (event.code === 'ArrowRight') {
+  refs.lightboxImg.getAttribute('alt');
+  }
+  // if (event.code === 'ArrowLeft') {
+  //   refs.lightboxImg.getAttribute('src', source-1);
+  //   refs.lightboxImg.getAttribute('alt', altName-1);
+  // }
+}
+
+refs.imagesBox.addEventListener('click', onImageClick);
+refs.lightboxCloseBtn.addEventListener('click', forCloseModal);
+refs.lightboxOverlay.addEventListener('click', forCloseModal);
